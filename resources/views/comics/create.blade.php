068f0b04d7623a9fe489ded6cf7628d6d3d90e56
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-26 17:15:24 +0200

    form page

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,7 +4,46 @@
 
     <div class="container">
 
-        ciao
+        <form action="">
+
+            <div>
+                <label for="title">Titolo</label>
+                <input type="text" name="title" placeholder="titolo">
+    
+                <label for="thumb">Copertina</label>
+                <input type="text" name="thumb" placeholder="Copertina">
+            </div>
+
+            <div>
+                <label for="series">Serie</label>
+                <input type="text" name="series" placeholder="Serie">
+
+                <label for="type">Tipologia</label>
+                <select name="type" id="type">
+                    <option value="">Seleziona</option>
+                    <option value="comic book">Comic Book</option>
+                    <option value="graphic novel">Graphic Novel</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="description">Descrizione</label>
+                <textarea name="description" id="description" cols="30" rows="10">Inserisci Descrizione</textarea>
+            </div>
+
+            <div>
+                <label for="sale_date">Data di uscita</label>
+                <input type="date" name="sale_date">
+    
+                <label for="price">Prezzo €</label>
+                <input type="number" name="price" min="0" max="999.99" step=".01">
+            </div>
+
+            <div>
+                <button>Crea</button>
+            </div>
+
+        </form>
 
     </div>
     
