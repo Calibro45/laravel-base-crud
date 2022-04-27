5c73605cde5bc46a668bc947936d235d1598942e
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-27 18:11:59 +0200

    show style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,52 +3,55 @@
 @section('content')
 
     <div class="container">
+        <div class="row">
+            
+            <figure class="col-4">
+                <img src="{{ $comic->thumb }}">
+            </figure>
+
+            <div class="col-6">
+                <h1>{{$comic->title}}</h1>
+
+                <div>
+                    <p>{{ $comic->description }}</p>
+                </div>
+
+                <div>
+                    <h2>Informazioni</h2>
         
-        <h1>{{$comic->title}}</h1>
+                    <ol>
+                        <li>
+                            Series: {{ $comic->series }}
+                        </li>
+                        <li>
+                            Tipologia: {{ $comic->type }}
+                        </li>
+                        <li>
+                            Data di Uscita: {{ $comic->sale_date }}
+                        </li>
+                        <li>
+                            Prezzo: € {{ $comic->price }}
+                        </li>
+                    </ol>
+                </div>
 
-        <figure>
-            <img src="{{ $comic->thumb }}">
-        </figure>
+                <div class="d-flex justify-content-end">
+                    <button class="btn btn-light mr-3">
+                        <a href="{{ route('comics.edit', $comic) }}">modifica</a>
+                    </button>
+ 
+                    <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+        
+                        <button type="submit" class="btn btn-light">
+                            Elimina
+                        </button>
+                    </form>
+                </div>
 
-        <div>
-            <h2>Descrizione</h2>
-            <p>{{ $comic->description }}</p>
+            </div>
         </div>
-
-        <div>
-            <h2>Informazioni</h2>
-
-            <ol>
-                <li>
-                    Series: {{ $comic->series }}
-                </li>
-                <li>
-                    Tipologia: {{ $comic->type }}
-                </li>
-                <li>
-                    Data di Uscita: {{ $comic->sale_date }}
-                </li>
-                <li>
-                    Prezzo: € {{ $comic->price }}
-                </li>
-            </ol>
-        </div>
-
-        <div>
-            <button>
-                <a href="{{ route('comics.edit', $comic) }}">modifica</a>
-            </button>
-        </div>
-
-        <div>
-            <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-                @csrf
-                @method('DELETE')
-
-                <button type="submit">Elimina</button>
-            </form>
-        </div>
-
     </div>
 
 @endsection
